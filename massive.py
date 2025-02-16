9117009794479a45bf738af70620c1e19330e29c
PA1pi4 <temavi2005@mail.ru>
2025-02-16 15:48:35 +0000

    Занятие 1.3

--- massive.py
+++ massive.py
@@ -1,18 +1,19 @@
-arr = [9, 7, 3, 2, 5, 254, 6,7]
-min = arr[0]  # 9
-min_old = min  # 9
+arr = [1, 2, 3, 4, 5, 6, 7] # Оригинальный массив
 
-for x in arr:  # 7
-    if x < min < min_old:  # 7| < 9 < 9
-        min_old = min # 14
-        min = x # 6
-    elif min < x < min_old: # 9 < 7| < 9
+min = arr[0]
+min_old = min
+
+for x in arr:
+    if x < min <= min_old:
+        min_old = min
+        min = x
+    elif min < x < min_old:
         min = min
         min_old = x
-    elif min < min_old < x: # 9 < 9 < |7
-        min = min #
-        min_old = min_old #
+    elif min < min_old < x:
+        min = min
+        min_old = min_old
     else:
-        min_old = x # 9
+        min_old = x
 
 print(min_old)
